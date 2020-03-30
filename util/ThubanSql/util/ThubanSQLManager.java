be01fa36da99f7de5d92509cdfd170055491cb6d
hahacandy <58638224+hahacandy@users.noreply.github.com>
2020-03-30 14:58:27 +0900

    Update ThubanSQLManager.java

--- util/ThubanSql/util/ThubanSQLManager.java
+++ util/ThubanSql/util/ThubanSQLManager.java
@@ -7,7 +7,6 @@ public class ThubanSQLManager {
 
 	//DBCP POOL 을 사용한 DB접속
 	public Connection getConnection() {
-
 		Connection conn = null;
 		
 		try {
@@ -32,21 +31,7 @@ public class ThubanSQLManager {
 			conn = getConnection();
 			pstmt = conn.prepareStatement(query);
 
-			if (options != null) {
-				List<String> options2 = inputListInfos(options);
-				for (int i = 1; i <= options2.size(); i++) {
-					option = options2.get(i - 1).split(",");
-					if (option[0].equalsIgnoreCase("str")) {
-//						System.out.println(option[1]);
-						pstmt.setString(i, option[1]);
-					}
-
-					else if (option[0].equalsIgnoreCase("int"))
-						pstmt.setInt(i, Integer.valueOf(option[1]));
-					else
-						System.out.println("카운트 쿼리 오류:" + i + " 번째 옵션");
-				}
-			}
+			inputPstmt(pstmt, options);
 			
 			rs = pstmt.executeQuery();
 			
@@ -72,27 +57,13 @@ public class ThubanSQLManager {
 		ResultSet rs = null;
 		
 		int row = 0;
-		String[] option = null;
 
 		try {
 			conn = getConnection();
 			pstmt = conn.prepareStatement(query);
-
-			if (options != null) {
-				List<String> options2 = inputListInfos(options);
-				for (int i = 1; i <= options2.size(); i++) {
-					option = options2.get(i - 1).split(",");
-					if (option[0].equalsIgnoreCase("str")) {
-//						System.out.println(option[1]);
-						pstmt.setString(i, option[1]);
-					}
-
-					else if (option[0].equalsIgnoreCase("int"))
-						pstmt.setInt(i, Integer.valueOf(option[1]));
-					else
-						System.out.println("업데이트 쿼리 오류:" + i + " 번째 옵션");
-				}
-			}
+			
+			inputPstmt(pstmt, options);
+			
 			row = pstmt.executeUpdate();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -114,14 +85,36 @@ public class ThubanSQLManager {
 		for (Object info : IInfos) {
 			cnt++;
 			if (info instanceof String) {
-				infos.add("str," + info);
+				infos.add("str$%#$%" + info);
 			} else if (info instanceof Integer) {
-				infos.add("int," + String.valueOf(info));
+				infos.add("int$%#$%" + String.valueOf(info));
 			} else {
 				System.out.println(cnt + "번째 리스트 값 입력 오류: " + String.valueOf(info));
 			}
 		}
 		return infos;
+	}
+	
+	protected void inputPstmt(PreparedStatement pstmt, List<Object> options) throws Exception{
+		
+		if (options != null) {
+			
+			String[] option = null;
+			
+			List<String> options2 = inputListInfos(options);
+			for (int i = 1; i <= options2.size(); i++) {
+				option = options2.get(i - 1).split("$%#$%");
+				if (option[0].equalsIgnoreCase("str")) {
+//					System.out.println(option[1]);
+					pstmt.setString(i, option[1]);
+				}
+
+				else if (option[0].equalsIgnoreCase("int"))
+					pstmt.setInt(i, Integer.valueOf(option[1]));
+				else
+					System.out.println("쿼리 매개변수 오류:" + i + " 번째 옵션");
+			}
+		}
 	}
 	
 	
@@ -155,21 +148,7 @@ public class ThubanSQLManager {
 			conn = getConnection();
 			pstmt = conn.prepareStatement(query);
 			
-			if (options != null) {
-				List<String> options2 = inputListInfos(options);
-				for (int i = 1; i <= options2.size(); i++) {
-					option = options2.get(i - 1).split(",");
-					if (option[0].equalsIgnoreCase("str")) {
-//						System.out.println(option[1]);
-						pstmt.setString(i, option[1]);
-					}
-
-					else if (option[0].equalsIgnoreCase("int"))
-						pstmt.setInt(i, Integer.valueOf(option[1]));
-					else
-						System.out.println("셀렉트 쿼리 오류:" + i + " 번째 옵션");
-				}
-			}
+			inputPstmt(pstmt, options);
 
 			rs = pstmt.executeQuery();
 			
