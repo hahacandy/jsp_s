51ce79d7aea0db67d0d4064600f2a227c2469191
hahacandy <58638224+hahacandy@users.noreply.github.com>
2020-04-07 09:24:52 +0900

    Update ThubanCookie.java

--- util/ThubanCookie/ThubanCookie.java
+++ util/ThubanCookie/ThubanCookie.java
@@ -39,11 +39,26 @@ public class ThubanCookie {
 	}
 	
 	//cookieName 쿠키이름, cookieSetValue 값을 넣으면 그대로 적용됨
-		public static void cookieSet(HttpServletRequest request, HttpServletResponse response, String cookieName, String cookieSetValue) {
-			
-			Cookie temp = new Cookie(cookieName, cookieSetValue);
-			temp.setMaxAge(60*60*24);
-			response.addCookie(temp);
-			
+	public static void cookieSet(HttpServletRequest request, HttpServletResponse response, String cookieName, String cookieSetValue) {
+
+		Cookie temp = new Cookie(cookieName, cookieSetValue);
+		temp.setMaxAge(60*60*24);
+		response.addCookie(temp);
+
+	}
+	
+		//해당 이름을 가진 쿠키의 값을 가져옴
+	public static String getCookie(HttpServletRequest request, HttpServletResponse response, String cookieName) {
+		
+		Cookie[] cookies = request.getCookies();
+		String cookieValues = null;
+
+		for(Cookie cookie : cookies) {
+			if(cookie.getName().equals(cookieName)) {
+				cookieValues = cookie.getValue();
+				return cookieValues;
+			}
 		}
+		return null;
+	}
 }
