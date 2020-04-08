86088b91605dd15f88d2d21e4694c4d57aae896b
hahacandy <58638224+hahacandy@users.noreply.github.com>
2020-04-08 22:40:44 +0900

    Update ThubanKanjiToFurigana.java

--- util/ThubanKanjiToFurigana/ThubanKanjiToFurigana.java
+++ util/ThubanKanjiToFurigana/ThubanKanjiToFurigana.java
@@ -28,6 +28,8 @@ public class ThubanKanjiToFurigana {
 			url.append("?appid="+api_key);
 			url.append("&sentence="+kanji);
 			
+			System.out.println(url);
+			
 			DocumentBuilderFactory dbFactoty = DocumentBuilderFactory.newInstance();
 			DocumentBuilder dBuilder = dbFactoty.newDocumentBuilder();
 			Document doc = dBuilder.parse(url.toString());
@@ -42,7 +44,10 @@ public class ThubanKanjiToFurigana {
 				if(nNode.getNodeType() == Node.ELEMENT_NODE){
 									
 					Element eElement = (Element) nNode;
-					result.append(getTagValue("Furigana", eElement));
+					try {
+						result.append(getTagValue("Furigana", eElement));
+					} catch (Exception e) {}
+					
 					//System.out.println("######################");
 					//System.out.println("Surface  : " + getTagValue("Surface", eElement));
 					//System.out.println("Furigana  : " + getTagValue("Furigana", eElement));
