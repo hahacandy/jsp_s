2d5fb8570cdce5030a565ef6dc9fec5c4fc7ffdb
hahacandy <58638224+hahacandy@users.noreply.github.com>
2020-04-15 04:00:41 +0900

    Update ThubanPasswd.java

--- util/Thuban KISA SHA256/ThubanPasswd.java
+++ util/Thuban KISA SHA256/ThubanPasswd.java
@@ -1,9 +1,10 @@
 //classes->KISA(key.dat, SHA256.class) is required
 
-import java.util.Random;
-
 import KISA.SHA256;
 import sun.misc.BASE64Encoder;
+
+import java.util.Random;
+
 
 public class ThubanPasswd {
 	
@@ -20,6 +21,7 @@ public class ThubanPasswd {
 	public static String createTempPasswd(int passLen) {
 		char[] passwdChars = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
 				'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+				'1','2','3','4','5','6','7','8','9','0',
 				'!','@','#','$','%','^','&','*'};
 
 		Random random = new Random();
@@ -34,3 +36,4 @@ public class ThubanPasswd {
 	
 	
 }
+
