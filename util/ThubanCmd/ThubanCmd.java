f5970d18e300af1ffd745e896c45a90c9333ab16
hahacandy <58638224+hahacandy@users.noreply.github.com>
2020-04-16 22:25:39 +0900

    Update ThubanCmd.java

--- util/ThubanCmd/ThubanCmd.java
+++ util/ThubanCmd/ThubanCmd.java
@@ -28,11 +28,13 @@ public class ThubanCmd {
 	            }
 	        };
 	        th.start();
+	        p.waitFor(); //이걸 써줘야 cmd명령어가 끝나야 넘어감
 	    } catch (IOException ex) {
 	        System.out.println("Error 1 : " + ex.getMessage());
 	    } catch (Exception ex) {
 	        System.out.println("Error 2 : " + ex.getMessage());
 	    }
+	    
 	}
 	
 }
